88d7701a4a6fb8405c04200891006cda18584ce8
HareemFarooq <hareemfarooq.qc@gmail.com>
2024-11-26 03:21:26 +0500

    Delete script_hareem.js.js

--- script_hareem.js.js
+++
@@ -1,8 +0,0 @@
-
-// Add simple fade-in animations on scroll (to be extended)
-document.addEventListener('DOMContentLoaded', () => {
-    const fadeElements = document.querySelectorAll('.fade');
-    fadeElements.forEach(el => {
-        el.style.opacity = 1;
-    });
-});
